c11e36d3061d9c6af4cc0610c81fa5880a41d708
ROYALPALACE88 <royal88cs@gmail.com>
2024-11-13 06:31:57 +0000

    fixed paragraph

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,3 @@ st.write(
     "ROYAL88 menghadirkan berbagai jenis slot, seperti slot klasik, slot video, dan jackpot progresif. Kami juga menawarkan promosi menarik seperti bonus deposit, cashback, dan program loyalitas bagi pemain setia. Proses transaksi di ROYAL88 sangat cepat dan aman, memberikan Anda kemudahan dalam melakukan deposit dan penarikan kapan saja.")
 st.write(
     "Bergabunglah dengan ROYAL88 sekarang dan rasakan pengalaman bermain yang luar biasa dengan beragam fitur keamanan dan permainan menarik. Jadilah bagian dari komunitas kami dan nikmati keuntungan serta promo eksklusif yang hanya tersedia di ROYAL88!")
-)
